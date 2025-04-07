70a95c81aeebcaaa9b99ff8a17d564a0f0bd57a7
Shefat-Shohan <shohan.ks3@gmail.com>
2025-04-08 05:12:24 +0600

    fix db

--- configs/index.ts
+++ configs/index.ts
@@ -1,7 +1,5 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-const sql = neon(
-  "postgresql://formio_owner:8CH6MRtIFTUJ@ep-billowing-band-a59qr6tz.us-east-2.aws.neon.tech/formio?sslmode=require"
-);
+const sql = neon(`${process.env.DATABASE_URL_CONFIG}`);
 export const db = drizzle(sql, { schema });
